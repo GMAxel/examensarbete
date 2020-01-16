e1020541913f64ebdae2a00177def75b47141c92
GMAxel <axel.tryde.at@gmail.com>
2020-01-16 17:53:01 +0100

    Snabb liten ändring

--- src/components/mainContent/chat/Chat.js
+++ src/components/mainContent/chat/Chat.js
@@ -102,7 +102,7 @@ const Chat = () => {
                         }
                         return (
                             <div 
-                                class={author} 
+                                className={author} 
                                 key={index}
                             >
 
